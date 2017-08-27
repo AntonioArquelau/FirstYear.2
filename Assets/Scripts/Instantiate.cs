64cfff1621d080af078175358dc1d76c066d54c6
Antônio Arquelau <antonio.arquelau@gmail.com>
2017-08-27 19:05:39 -0300

    first commit

---
+++ Assets/Scripts/Instantiate.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiate : MonoBehaviour {
+	public GameObject prefab;
+	public float delay;
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("InstantiatePrefab", 0, delay);
+	}
+	
+	// Update is called once per frame
+	void InstantiatePrefab () {
+		Instantiate (prefab);
+	}
+}
---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour {
+	private Rigidbody2D rigidBody2D;
+	private Vector2 force;
+	private bool canJump;
+	// Use this for initialization
+	void Start () {
+		rigidBody2D = gameObject.GetComponent<Rigidbody2D> ();
+		force = new Vector2 (0, 250);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0) && canJump) {
+			rigidBody2D.AddForce(force);
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D obj){
+		if (obj.gameObject.tag.Equals ("Ground")) {
+			canJump = true;
+		}
+				
+	}
+	void OnCollisionExit2D(Collision2D obj){
+		if (obj.gameObject.tag.Equals ("Ground")) {
+			canJump = false;
+		}
+
+	}
+}
