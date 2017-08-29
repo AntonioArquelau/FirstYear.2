7fd381346284f999b538d41fe8352a8cd4faa518
Antônio Arquelau <antonio.arquelau@gmail.com>
2017-08-28 23:34:48 -0300

    Load Scene

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadScene : MonoBehaviour {
+
+	public void LoadCena(){
+		Application.LoadLevel ("Eduardo");
+	}
+}
